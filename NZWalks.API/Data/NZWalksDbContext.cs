02415d1f830b7b6e232c23dd0129a884aa735c90
Abdelrahman <abdoahmed424@gmail.com>
2024-08-19 01:04:42 +0300

    Init the project with EFCore and Models

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Walk>  Walks { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
