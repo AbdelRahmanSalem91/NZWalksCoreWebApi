ffe9378297b65c7875181c0cb50b194c2ed0e826
Abdelrahman <abdoahmed424@gmail.com>
2024-08-19 01:25:27 +0300

    cleaning

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cefc1e5a8d0be96fc32a98656192e531db892dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3424dcc8056175fa69c3aae5f56c909a076fc338")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
